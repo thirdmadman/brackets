08ebf76c5a3846c39ccfe272c87f48c63e9e50d0
thirdmadman <thirdmadman@gmail.com>
2020-10-25 23:14:04 +0400

    init: impl fancy triple forEach function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let gg = str;
+    bracketsConfig.forEach(() => {bracketsConfig.forEach(() => { bracketsConfig.forEach(
+        e => {
+            while (gg.indexOf(e[0] + e[1])>=0) {
+                gg = gg.replace(e[0] + e[1], "");
+            }
+        }
+    );});});
+    return gg.length === 0;
 }
